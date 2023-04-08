a45c6d5dd4874f835bafefbee65d0691f4616215
alessandro-001 <alessandrofrondini@gmail.com>
2023-04-08 18:45:03 +0100

    updated filter method

--- _filter().js
+++ _filter().js
@@ -7,7 +7,6 @@ the callback function that return either true or false, whether that item should
 in the new filtered array.
 
 */
-
 const animals = [
     { name: "Fluffy",   species: "rabbit" },
     { name: "Caro",     species: "dog" },
@@ -18,19 +17,20 @@ const animals = [
 ];
 
 // // if implementing a for loop would be:
-// let dogs = [];
-// for (let i = 0; i < animals.length; i++) {
-//     if (animals[i].species === "dog") {
-//         dogs.push(animals[i]);
-//     }
-// }
+let dogs = [];
+for (let i = 0; i < animals.length; i++) {
+    if (animals[i].species === "dog") {
+        dogs.push(animals[i]);
+    }
+}
+console.log(dogs);
 
 // with the FILTER method instead:
-
-let isDog = function(animal) {    
+let dogs2 = animals.filter(function(animal) {    
     return animal.species === "dog";
-}
+}); 
+console.log(dogs2);
 
-let dogs = animals.filter(isDog); 
-
-console.log(dogs)
+// can also type it like this:
+let dogs3 = animals.filter( animal => animal.species === "dog" )
+console.log(dogs3);
